008813abada5116434624bfec6021723af54354a
XValidator <75050044+XValidator@users.noreply.github.com>
2025-06-10 19:51:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from telegram.ext import ApplicationBuilder, CommandHandler
+from mind_wallet_bot.config import BOT_TOKEN
+from mind_wallet_bot.handlers.wallet import wallet_handler
+from mind_wallet_bot.handlers.balance import balance_handler
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("wallet", wallet_handler))
+app.add_handler(CommandHandler("balance", balance_handler))
+
+if __name__ == "__main__":
+    print("🤖 Бот запущено...")
+    app.run_polling()
